27258d4c1843dd2f9f63772b6740ff1f71a99d64
Thomas Ballinger <thomasballinger@gmail.com>
2014-02-12 11:59:07 -0500

    add turn

--- ttt.py
+++ ttt.py
@@ -3,6 +3,8 @@
 class Board(object):
     def __init__(self):
         self.rows = [[0] * 3 for _ in range(3)]
+    def turn(self):
+        return sum(len(spot for spot in row if spot != 0) for row in self.rows)
 
 
 
