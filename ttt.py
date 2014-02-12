2a42612580a2a5bbb5335c9262dd50da9188cb34
Thomas Ballinger <thomasballinger@gmail.com>
2014-02-12 11:36:14 -0500

    added a display method

--- ttt.py
+++ ttt.py
@@ -1,6 +1,9 @@
 
 
 class Board(object):
+    sigils = {1:'x',
+              -1:'0',
+              0:'.',}
     def __init__(self):
         self.rows = [[0] * 3 for _ in range(3)]
     @property
@@ -9,6 +12,11 @@ class Board(object):
     @property
     def spots(self):
         return sum(self.rows, [])
+    def __str__(self):
+        return '\n'.join(' '.join(Board.sigils[s] for s in row) for row in self.rows)
 
+if __name__ == '__main__':
+    b = Board()
+    print b
 
 # this is a fancy class
